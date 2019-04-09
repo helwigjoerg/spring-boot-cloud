57ad57dda49cbccbcb21aaf7a2a5b3b85bbecfa2
Joerg Helwig <helwigjoerg@gmail.com>
2019-04-09 12:39:58 +0200

    update to Greenwich.SR1

--- cloud-spring-cloud-config-server/src/test/java/com/helwigjoerg/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ cloud-spring-cloud-config-server/src/test/java/com/helwigjoerg/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.springcloudconfigserver;
+package com.helwigjoerg.microservices.springcloudconfigserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
